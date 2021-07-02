2cf3a49aacc9f4ba405dfb8cd23671679dc5a99c
hack-tramp <53376845+hack-tramp@users.noreply.github.com>
2021-07-02 17:11:51 +0100

    Add files via upload

--- wnetwrap.h
+++ wnetwrap.h
@@ -1,27 +1,48 @@
 /*
+-------------------------
+----------------TO-DO----
+-------------------------
+--------sessions
+todo: preserve session until UA or proxy change - dont use InternetOpen again unless params have been changed
+DONE: but session is never closed - does it automatically get closed when return 0 is called at the end of main? create global class with destructor?
+todo: allow multiple sessions/connections https://stackoverflow.com/questions/48651185/why-two-calls-to-httpsendrequest-on-different-connections-new-internetopen-and#49061318
 
-todo: cookies map response
-todo: send cookies with URL encoding
-todo: preserve session until host change
-todo: make header resp case insensitive
+---------proxy 
+todo: error catching for setting proxy usr/pwd
+todo: function for user to clear proxy (reason for creating session obj?)
+todo: encode proxy usr/pwd - is this needed or does WinINet do it? (test + check)
+todo: see option flags INTERNET_OPTION_PER_CONNECTION_OPTION "To change proxy information for the entire process without affecting the global settings..."
+https://docs.microsoft.com/en-us/windows/win32/wininet/option-flags
 
-process redirects manually? to allow cookies, bytes sent/recd, and redirect count to be accurate
-fix: auto redirect or not? INTERNET_FLAG_NO_AUTO_REDIRECT https://stackoverflow.com/questions/10707325/wininet-how-to-obtain-server-url-after-301-redirect
+-------cookies
+todo: set-cookies recd must take into account other = than name <----- TEST
 
-fix: accept gzip data... see this async wrapper that does it: https://www.codeproject.com/articles/43860/an-asynchronous-http-download-class-for-mfc-atl-an
-fix: HttpOpenRequestA and HttpSendRequestA seem to work passing utf-8 params even though the docs recommend always using the W commands
+--------redirects
+todo: when redirects are off, allow it to pull all info, status code headers etc
+todo: redirect adjust method e.g. POST to GET https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Location
 
-notes
-- cookies currently only set for url without path (domain only)
-- no cookie url encoding
+---------testing needed:
+test proxy
+test abs redirect
+test cookies with more attributes data than just cookie name=value
+test cookies with encoding off
+
+todo: accept gzip data... see this async wrapper that does it: https://www.codeproject.com/articles/43860/an-asynchronous-http-download-class-for-mfc-atl-an
+
+
+
+-------------------------
+----------------NOTES----
+-------------------------
+- cookies only set for url without path (domain only) unless domain or path is set in cookie data
 - session cookies not preserved
 
-- in url only query params, not host / path is url encoded
-- due to MS bug timeout is done via worker thread, this means it cant be increased beyond MS default
-also timeouts might cause memory leaks, are all threads, pointers, structs, vars etc deleted/freed? 
-according to this SO thread no need to delete or free unless new or malloc called https://stackoverflow.com/questions/5243360/how-to-free-memory-for-structure-variable
-should smart pointers be used in the workers? the docs mainly talk about cases where new is used https://docs.microsoft.com/en-us/cpp/cpp/smart-pointers-modern-cpp?view=msvc-160
+- url encoding of space is + not %20
 
+- HttpOpenRequestA and HttpSendRequestA seem to work passing utf-8 params even though the docs recommend always using the W commands
+- in url only query params, not host / path is url encoded 
+However docs suggest host is auto encoded: "By default, the host or authority portion of the URL is encoded according to the IDN specification for both direct and proxy connections. "
+https://docs.microsoft.com/en-us/windows/win32/wininet/option-flags -> INTERNET_OPTION_IDN
 
 fixed: post data size limit - fixed by converting postdata string to char array before sending
 */
@@ -41,6 +62,8 @@ fixed: post data size limit - fixed by converting postdata string to char array 
 #include <map>
 #include <regex>
 #include <initializer_list>
+#include <algorithm> 
+#include <cctype>
 #include <locale>
 #include <codecvt>
 #include <stdlib.h>
@@ -51,7 +74,29 @@ fixed: post data size limit - fixed by converting postdata string to char array 
 
 
 
+
 namespace wrap {
+
+
+
+	struct Comparator {
+		bool operator() (const std::string& s1, const std::string& s2) const {
+			std::string str1;
+			std::string str2;
+			//std::transform(s1.begin(), s1.end(), str1.begin(), tolower);
+			for (auto& c : s1)
+			{
+				str1 += std::tolower(c);
+			}
+			for (auto& c : s2)
+			{
+				str2 += std::tolower(c);
+			}
+
+			//std::transform(s2.begin(), s2.end(), str2.begin(), tolower);
+			return  str1 < str2;
+		}
+	};
 
 	struct Body { //used for postdata
 		std::string body;
@@ -73,6 +118,10 @@ namespace wrap {
 		std::string adr;
 	};
 
+	struct UserAgent {
+		std::string usr;
+	};
+
 	struct Header {
 		std::map<std::string, std::string> hdr;
 
@@ -86,6 +135,13 @@ namespace wrap {
 
 		Parameters(std::initializer_list<std::pair<const std::string, std::string> > p) :
 			p(p) {
+		}
+	};
+
+	struct Cookies {
+		std::map<std::string, std::string> c;
+		Cookies(std::initializer_list<std::pair<const std::string, std::string> > c ) :
+			c(c) {
 		}
 	};
 
@@ -119,50 +175,58 @@ namespace wrap {
 		std::string type = "request"; // connection or request
 	};
 
+	struct Options {
+		std::map<std::string, std::string> options;
+		Options(std::initializer_list<std::pair<const std::string, std::string> > options) :
+			options(options) {
+		}
+	};
+
+	struct Proxy {
+		std::string url;
+		std::string usr = "";
+		std::string pwd = "";
+	};
+
 	struct req {
 
 		std::string ua = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0";
 		std::string Params;
 		std::string PostData; // key is type of data (body,payload or file) and value is the data
-		std::string Cookies;
 		std::string Dl;
-		std::string Proxies;
 		std::string Method = "GET";
 		std::string Auth;
 		std::string Url = "www.example.com";
+		wrap::Proxy Proxy;
+		std::map <std::string, std::string> cookies_tosend;
+		bool cookie_encode = true;
+		bool redirect = true;
+		bool newsession = true; //if current session should be replaced with a new one via InternetOpen - triggered by proxy or user agent changes
+		HINTERNET session_handle = NULL;
 		Header Header = { {"",""} };
 		DWORD TimeoutConnect = 0;
 		DWORD TimeoutRequest = 0;
+		DWORD AccessType = INTERNET_OPEN_TYPE_DIRECT;
 		void reset() {
 			ua = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0";
 			Params = "";
 			PostData = "";
-			Cookies = "";
 			Dl = "";
-			Proxies = "";
 			Method = "GET";
 			Auth = "";
 			Url = "www.example.com";
 			Header = { {"",""} };
+			//cookies_tosend.clear(); - currently done at end of prepare_cookies()
 			TimeoutConnect = 0;
 			TimeoutRequest = 0;
+			newsession = false;
+			cookie_encode = true;
+			redirect = true;
+
 		};
 	};
 
-	struct Comparator {
-		bool operator() (const std::string& s1, const std::string& s2) const {
-			std::string str1;
-			std::string str2;
-			//std::transform(s1.begin(), s1.end(), str1.begin(), tolower);
-			for (auto& c : s1)
-			{str1 += std::tolower(c);}
-			for (auto& c : s2)
-			{str2 += std::tolower(c);}
 
-			//std::transform(s2.begin(), s2.end(), str2.begin(), tolower);
-			return  str1 < str2;
-		}
-	};
 
 	struct Response {
 		std::map <std::string, std::string, Comparator> header;
@@ -190,6 +254,8 @@ namespace wrap {
 
 	void Params(wrap::Url u);
 
+	void Params(wrap::UserAgent usr);
+
 	void Params(wrap::Header h);
 
 	void Params(wrap::Payload pd);
@@ -206,9 +272,15 @@ namespace wrap {
 
 	void Params(wrap::Body body);
 
+	void Params(wrap::Cookies c);
+
 	void Params(wrap::File file);
 
 	void Params(wrap::Timeout timeout);
+
+	void Params(wrap::Options options);
+
+	void Params(wrap::Proxy proxy);
 
 	// url, headers, params, postdata, cookies, dl flag, proxies, timeout, method, auth 
 	Response httpsreq(req Request);
